f0928b086059c896c763244c00e04a3127ea830b
saiteja028 <saitejakatta123@gmail.com>
2026-01-01 10:33:25 +0530

    Added microservices

---
+++ product-service/src/test/java/com/sai/ordersystem/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.ordersystem.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
